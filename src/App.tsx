d776a78080d148631d51085bf39864c7753d2dbf
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-01-09 18:40:52 +0100

    add project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom"
+import Consultation08 from "./consultations/Consultation08/Consultation08"
+
+//lessons
+
+//homeworks
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Consultation08 />
+    </BrowserRouter>
+  )
+}
+
+export default App
